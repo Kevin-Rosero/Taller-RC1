d10ab084431ca185e19ea3f3996170d9ba2493c5
Kevin-Rosero <kevin.rosero.herrera@udla.edu.ec>
2025-11-12 19:06:08 +0000

    Commit inicial

---
+++ main.c
@@ -0,0 +1,16 @@
+/* main.c - Programa principal simple */
+//Instrucciones:
+/*1. El programa debe tener un arreglo para los recursos disponibles (inventario).
+  2. Hay que hacer una tabla con los recursos que gasta hacer cada acción, incluido el tiempo que cuesta.
+  3. Al ingresar el pedido, imprimir na tabla con los recursos que se van a usar. Si no, validar que no se puede.
+  4. Después de validar, hacer que imprima un mensaje de confirmación y restar los recursos usados.*/
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    const char *version = "0.1.0";
+
+    
+    
+    return 0;
+}
