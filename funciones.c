c50cb7778c6c19b8b323d76c939acc52fe83d046
Kevin-Rosero <kevin.rosero.herrera@udla.edu.ec>
2025-11-14 19:19:26 -0600

    Fix comment typo in buscarProducto function

--- funciones.c
+++ funciones.c
@@ -11,7 +11,7 @@
 int buscarProducto(char nombres[][MAX_NOMBRE], int cont, char buscado[]) {
     for (int i = 0; i < cont; i++) {
         if (strcmp(nombres[i], buscado) == 0) {
-            return i; // posición/producto encontrado
+            return i; // posicion/producto encontrado
         }
     }
     return -1; // no encontrado
